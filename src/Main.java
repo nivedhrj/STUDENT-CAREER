dfa72ce74d26c7d5035f82eff1e1bf5b07074cd2
nivedhrj <nivedhrj2009@gmail.com>
2024-08-05 19:30:37 +0530

    ninth commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class Main{
+    int mm;
+    int scm;
+    int com;
+    int total;
+    double per;
+    Main(int mm, int scm,int com){
+       this.mm=mm;
+       this.scm=scm;
+       this.com=com;
+
+    }
+    public void input(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your marks in MATHS.:");
+        mm=sc.nextInt();
+        System.out.println("Enter your marks in SCIENCE:");
+        scm=sc.nextInt();
+        System.out.println("Enter your marks in COMPUTER SCIENCE:");
+        com=sc.nextInt();
+        sc.close();
+    }
+    public void check(){
+        total=mm+scm+com;
+        per=(total/1000.0)*100;
+        if(mm>=90&&scm>=90&&com>=90){
+            System.out.println("Science with Computer");
+        }
+        else if(per>90){
+            System.out.println("Bio-Science");
+        }
+        else if(per>=80&&per<90){
+            System.out.println("Science with Hindi");
+        }
+        else{
+            System.out.println("You are not eligible.");
+        }
+    }
+    public static void main(String[]Args){
+        Main Main=new Main(0,0,0);
+        Main.input();
+        Main.check();
+    }
+}
